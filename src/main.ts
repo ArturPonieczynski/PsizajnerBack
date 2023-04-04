b111e78fda08900cd69d51bc6114c2c27678b0c5
Artur Ponieczynski <ponieczynski@gmail.com>
2023-04-04 23:59:09 +0200

    Added cookie and helmet middleware, updated tsconfig.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  (app as NestExpressApplication).use(helmet());
+
+  app.use(cookieParser());
+
+  await app.listen(3001);
 }
 bootstrap();
